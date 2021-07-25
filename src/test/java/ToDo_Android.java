71f3f0565376a9ef577a1e1dec927bc2c5f8cd5d
hala-samir <h.samir2011@gmail.com>
2021-07-25 10:36:13 +0200

    dataDrivenCommit

--- src/test/java/ToDo_Android.java
+++ src/test/java/ToDo_Android.java
@@ -1,18 +1,29 @@
+import PageObjects.AddTask;
+import PageObjects.TaskListPage;
+import org.json.simple.parser.ParseException;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import utils.JSONReader;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 
 public class ToDo_Android extends TestBase {
     AddTask addTaskPage;
     TaskListPage taskListPage;
-    @Test
-    public void test_add_task() throws MalformedURLException {
+    @DataProvider(name="tasks data")
+    public Object[][] passData() throws IOException, ParseException {
+        return JSONReader.jsonData(System.getProperty("user.dir")+"/Data/Data.json",
+                "TaskData",2);
+    }
+    @Test(dataProvider = "tasks data")
+    public void test_add_task(String taskName, String taskDesc) throws MalformedURLException {
         Android_setup();
         addTaskPage= new AddTask(driver);
         taskListPage= new TaskListPage(driver);
         taskListPage.clickAddTask();
-        addTaskPage.enterTaskTitle("Automation Task");
-        addTaskPage.enterNotes("Appium for Mobile");
+        addTaskPage.enterTaskTitle(taskName);
+        addTaskPage.enterNotes(taskDesc);
         //addTaskPage.enterTag("e2e");
         addTaskPage.clickSaveBtn();
         tearDown();
